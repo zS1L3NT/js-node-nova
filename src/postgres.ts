ee0180107ed0d0d69b4361f22d14530d1d8b96b3
Zechariah <zechariahtan144@gmail.com>
2022-09-09 23:59:24 +0800

    ✨ add postgres

---
+++ src/postgres.ts
@@ -0,0 +1,43 @@
+import "https://deno.land/x/dotenv@v3.2.0/load.ts"
+
+import {
+	Database, DataTypes, Model, PostgresConnector
+} from "https://deno.land/x/denodb@v1.0.40/mod.ts"
+
+const connection = new PostgresConnector({
+	uri: Deno.env.get("DATABASE_URL")!
+})
+
+export const db = new Database(connection)
+
+export class Config extends Model {
+	static table = "configs"
+	static timestamps = true
+
+	static fields = {
+		filename: {
+			type: DataTypes.STRING,
+			primaryKey: true
+		},
+		shorthand: DataTypes.STRING,
+		content: DataTypes.STRING
+	}
+}
+
+export class Secret extends Model {
+	static table = "secrets"
+	static timestamps = true
+
+	static fields = {
+		project: {
+			type: DataTypes.STRING,
+			primaryKey: true
+		},
+		path: DataTypes.STRING,
+		content: DataTypes.STRING
+	}
+}
+
+db.link([Config, Secret])
+
+// db.sync({ drop: true })
